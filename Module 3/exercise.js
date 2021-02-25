ed09bfce9c1f146bc281d54a2cc5e4e0ab248cfd
Ryan Lasauskas <ryan.lasauskas@doane.edu>
2021-02-24 19:48:19 -0600

    js attempt

---
+++ Module 3/exercise.js
@@ -0,0 +1,30 @@
+function validateANDadd(){
+
+  var theNewWord = document.forms["myForm"]["newWord"].value;
+  var theNewNumber = document.forms["myForm"]["newNumber"].value;
+
+  if(theNewWord == ""){
+    alert("Please Enter a Word to Check");
+    return false;
+  }
+
+  else if((theNewNumber != 1) && (theNewNumber != 2)){
+    alert("Please Enter a 1 or 2 For The List");
+    document.forms["myForm"]["newNumber"].value = "";
+    return false;
+  }
+
+  else{
+    if(theNewNumber==1){
+      var tableRef = document.getElementById("myList1");
+      (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;
+    } else{
+      var tableRef = document.getElementById("myList2");
+      (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;
+    }
+
+    document.forms["myForm"]["newWord"].value = "";
+    document.forms["myForm"]["newNumber"].value = "";
+    return true;
+  }
+}
