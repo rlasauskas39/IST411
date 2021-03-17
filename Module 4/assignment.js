b46ee05e6c9b8a3cd2e9ef191f414866ba9f8c5d
Ryan Lasauskas <ryan.lasauskas@doane.edu>
2021-03-16 21:57:31 -0500

    Doesnt separate paragraphs

--- Module 4/assignment.js
+++ Module 4/assignment.js
@@ -12,6 +12,7 @@ async function getBaconIpsum(){
   //Clear previous data
   document.getElementById("raw").innerHTML = "";
   document.getElementById("formatted").innerHTML = "";
+  document.getElementById("encrypted").innerHTML = "";
 
   //JSON formatting for response
   var json = await response.json();
@@ -22,6 +23,55 @@ async function getBaconIpsum(){
   //loop through and print each formatted paragraph
   for(var para in json){
     document.getElementById("formatted").innerHTML += "<p>" + json[para] + "</p>";
+  }
+ 
+
+
+  //Ciphers
+
+  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-'];
+
+  if(document.getElementById("algoSelect").value === "option1"){
+    //Cipher #1
+    let rawData = json
+
+    let lower = rawData.toLowerCase();
+
+    
+    //let lowerArr = lower.split("");
+    
+
+    
+    /*for(let i = 0; i < lowerArr.length; i++){
+      let a = alphabet.indexOf(lowerArr[i]);
+      let shift = a + 7;
+      lowerArr[i] = alphabet[shift];
+    }
+
+    let encryption = lowerArr.toString();
+
+    document.getElementById("encrypted").innerHTML = encryption;
+
+*/
+  }
+
+  else{
+    //Cipher #2
+    //random number generator for 0-27
+    
+    var cipher = [];
+    for(var para in json){
+      for(let i = 0; i < json[para].length; i++){
+        if(json[para][i] == " " || json[para][i] == "."){
+          cipher[i] = json[para][i];
+        } else{
+          let ran = Math.floor(Math.random() * 27);
+          cipher.push(alphabet[ran]);
+        }
+      }
+    }
+    cipher = cipher.join("");
+    document.getElementById("encrypted").innerHTML = cipher;
   }
   return true;
 }
