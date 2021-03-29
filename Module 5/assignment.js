c85c3494ce51d588a1639ad383b6a75ae3312d50
Ryan Lasauskas <ryan.lasauskas@doane.edu>
2021-03-28 23:35:13 -0500

    Exceptional Done

--- Module 5/assignment.js
+++ Module 5/assignment.js
@@ -1,30 +1,35 @@
 async function getQuote(){
-  var minLength;
-  var maxLength;
+  var minLength = "";
+  var maxLength = "";
+  var quote;
+  var author;
   var length = document.getElementById("lengthChosen").value;
-
-  //Loop through json and += options of each tag to another dropdown for optional tags
-  //Link to tags json https://api.quotable.io/tags
-  //Find two new bootstrap elements
+  var tag = document.getElementById("tags").value;
+  document.getElementById("alertLocation").innerHTML = "";
 
   if(length === "short"){
-    minLength = "";
-    maxLength = "maxLength=50";
+    minLength = "?minLength=0";
+    maxLength = "&maxLength=50";
   } else if(length === "medium"){
-    minLength = "minLength=50",
+    minLength = "?minLength=50",
     maxLength = "&maxLength=150";
-  } else{
-    minLength = "minLength=150";
+  } else if(length === "long"){
+    minLength = "?minLength=150";
     maxLength = "";
   }
 
-  var apiString = "https://api.quotable.io/random?" + minLength + maxLength;
-
+  var apiString = "https://api.quotable.io/random" + minLength + maxLength + tag;
   var response = await fetch(apiString);
   var json = await response.json();
 
-  var quote = json.content;
-  var author = json.author;
+  if(json.statusCode === 404){
+    quote = json.statusMessage;
+    author = "";
+    document.getElementById("alertLocation").innerHTML = "<div class='alert alert-danger'>Sorry, Could not find a quote for the selected parameters. Please Try a different combination.</div>";
+  } else{
+    quote = json.content;
+    author = json.author;
+  }
 
   document.getElementById("quoteLocation").innerHTML = quote;
   document.getElementById("authorLocation").innerHTML = author;
