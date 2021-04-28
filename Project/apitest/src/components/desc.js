946be709f0e38ad89ac13b62ed81eaee25247968
Ryan Lasauskas <ryan.lasauskas@doane.edu>
2021-04-28 01:13:19 -0500

    API working

--- Project/apitest/src/components/desc.js
+++ Project/apitest/src/components/desc.js
@@ -9,8 +9,17 @@ const Description = ({description}) => {
           <div className="card-body">
             <h5 className="card-title">{brewery.name}</h5>
             <h6 className="card-subtitle mb-2 text-muted">ID#: {brewery.id}</h6>
+            <p className="card-text">Type: {brewery.brewery_type}</p>
+            <p className="card-text">Address: {brewery.street}</p>
             <p className="card-text">City: {brewery.city}</p>
+            <p className="card-text">State: {brewery.state}</p>
             <p className="card-text">Postal: {brewery.postal_code}</p>
+            <p className="card-text">Country: {brewery.country}</p>
+            <p className="card-text">Phone: {brewery.phone}</p>
+            <p className="card-text">Website: <a href={brewery.website_url}>{brewery.website_url}</a></p>
+            <p className="card-text">Last Database Update: {brewery.updated_at}</p>
+            
+            
           </div>  
         </div>  
       ))}
