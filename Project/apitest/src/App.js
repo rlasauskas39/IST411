a79247220b71b936a646c0f131b9ff6cdbcf2283
Ryan Lasauskas <ryan.lasauskas@doane.edu>
2021-04-28 21:16:28 -0500

    styling

--- Project/apitest/src/App.js
+++ Project/apitest/src/App.js
@@ -35,196 +35,139 @@ class App extends Component{
     })
   }
 
+  /* 
+  May add:
+  Random Brewery button
+  search by ID number button
+  */
+
   render(){
     return(
       <div className="container">
-
-<header class="masthead text-white text-center">
-            <div class="overlay"></div>
+        <nav class="navbar navbar-light bg-light static-top">
             <div class="container">
-            <div class="col-xl-9 mx-auto"><h1 class="mb-5">Find Your Next Favorite Brewery Now!</h1></div>
+                <h2 class="navbar-brand">Brewery Locator</h2>
+            </div>
+        </nav>
+        <header class="masthead text-white text-center">
+          <div class="overlay"></div>
+            <div class="container">
+              <div class="col-xl-9 mx-auto"><h1 class="mb-5"><span class="mast-title">Find Your Next Favorite Brewery Now!</span></h1></div>
                 <div class="row">
                     
-                    <div class="col-lg-3">
-                      <div class="mx-auto mb-lg-0">
-                        <label for="userName">Name:</label>
-                        <input className="text" name="userName" id="userName" onChange={this.myChangeHandler}></input>
-                      </div>
+                  <div class="col-lg-3">
+                    <div class="mx-auto mb-lg-0">
+                      <label for="userName">Name:</label><br />
+                      <input type="text" name="userName" id="userName" placeholder="Search by Name" onChange={this.myChangeHandler}></input>
                     </div>
+                  </div>
 
-                    <div class="col-lg-3">
-                      <div class="mx-auto mb-lg-0">
-                        <label for="userCity">City:</label>
-                        <input className="text" name="userCity" id="userCity" onChange={this.myChangeHandler}></input>
-                      </div>
+                  <div class="col-lg-3">
+                    <div class="mx-auto mb-lg-0">
+                      <label for="userCity">City:</label><br />
+                      <input type="text" name="userCity" id="userCity" placeholder="Search by City" onChange={this.myChangeHandler}></input>
                     </div>
+                  </div>
 
-                    <div class="col-lg-3">
-                      <div class="mx-auto mb-lg-0">
-                        <label for="userType">Type: </label>
-                        <select id="userType" name="userType" onChange={this.myChangeHandler}>
-                          <option value="">Select A Type</option>
-                          <option value="micro">Micro</option>
-                          <option value="nano">Nano</option>
-                          <option value="regional">Regional</option>
-                          <option value="brewpub">Brewpub</option>
-                          <option value="large">Large</option>
-                          <option value="planning">Planning</option>
-                          <option value="bar">Bar</option>
-                          <option value="contract">Contract</option>
-                          <option value="proprietor">Proprietor</option>
-                          <option value="closed">Closed</option>
-                        </select>
-                      </div>
+                  <div class="col-lg-3">
+                    <div class="mx-auto mb-lg-0">
+                      <label for="userType">Type: </label><br />
+                      <select id="userType" name="userType" onChange={this.myChangeHandler}>
+                        <option value="">Search by Type</option>
+                        <option value="micro">Micro</option>
+                        <option value="nano">Nano</option>
+                        <option value="regional">Regional</option>
+                        <option value="brewpub">Brewpub</option>
+                        <option value="large">Large</option>
+                        <option value="planning">Planning</option>
+                        <option value="bar">Bar</option>
+                        <option value="contract">Contract</option>
+                        <option value="proprietor">Proprietor</option>
+                        <option value="closed">Closed</option>
+                      </select>
                     </div>
+                  </div>
 
-                    <div class="col-lg-3">
-                      <div class="mx-auto mb-lg-0">
-                      <label for="userState">State: </label>
-        <select id="userState" name="userState" onChange={this.myChangeHandler}>
-          <option value="">Select a State</option>
-          <option value="alabama">Alabama</option>
-          <option value="alaska">Alaska</option>
-          <option value="arizona">Arizona</option>
-          <option value="arkansas">Arkansas</option>
-          <option value="california">California</option>
-          <option value="colorado">Colorado</option>
-          <option value="connecticut">Connecticut</option>
-          <option value="delaware">Delaware</option>
-          <option value="florida">Florida</option>
-          <option value="georgia">Georgia</option>
-          <option value="hawaii">Hawaii</option>
-          <option value="idaho">Idaho</option>
-          <option value="illinois">Illinois</option>
-          <option value="indiana">Indiana</option>
-          <option value="iowa">Iowa</option>
-          <option value="kansas">Kansas</option>
-          <option value="kentucky">Kentucky</option>
-          <option value="louisiana">Louisiana</option>
-          <option value="maine">Maine</option>
-          <option value="maryland">Maryland</option>
-          <option value="massachusetts">Massachusetts</option>
-          <option value="michigan">Michigan</option>
-          <option value="minnesota">Minnesota</option>
-          <option value="mississippi">Mississippi</option>
-          <option value="missouri">Missouri</option>
-          <option value="montana">Montana</option>
-          <option value="nebraska">Nebraska</option>
-          <option value="nevada">Nevada</option>
-          <option value="new_hampshire">New Hampshire</option>
-          <option value="new_jersey">New Jersey</option>
-          <option value="new_mexico">New Mexico</option>
-          <option value="new_york">New York</option>
-          <option value="north_carolina">North Carolina</option>
-          <option value="north_dakota">North Dakota</option>
-          <option value="ohio">Ohio</option>
-          <option value="oklahoma">Oklahoma</option>
-          <option value="oregon">Oregon</option>
-          <option value="pennsylvania">Pennsylvania</option>
-          <option value="rhode_island">Rhode Island</option>
-          <option value="south_carolina">South Carolina</option>
-          <option value="south_dakota">South Dakota</option>
-          <option value="tennessee">Tennessee</option>
-          <option value="texas">Texas</option>
-          <option value="utah">Utah</option>
-          <option value="vermont">Vermont</option>
-          <option value="virginia">Virginia</option>
-          <option value="washington">Washington</option>
-          <option value="west_virginia">West Virginia</option>
-          <option value="wisconsin">Wisconsin</option>
-          <option value="wyoming">Wyoming</option>
-        </select>
-                      </div>
+                  <div class="col-lg-3">
+                    <div class="mx-auto mb-lg-0">
+                      <label for="userState">State: </label><br />
+                      <select id="userState" name="userState" onChange={this.myChangeHandler}>
+                        <option value="">Search by State</option>
+                        <option value="alabama">Alabama</option>
+                        <option value="alaska">Alaska</option>
+                        <option value="arizona">Arizona</option>
+                        <option value="arkansas">Arkansas</option>
+                        <option value="california">California</option>
+                        <option value="colorado">Colorado</option>
+                        <option value="connecticut">Connecticut</option>
+                        <option value="delaware">Delaware</option>
+                        <option value="florida">Florida</option>
+                        <option value="georgia">Georgia</option>
+                        <option value="hawaii">Hawaii</option>
+                        <option value="idaho">Idaho</option>
+                        <option value="illinois">Illinois</option>
+                        <option value="indiana">Indiana</option>
+                        <option value="iowa">Iowa</option>
+                        <option value="kansas">Kansas</option>
+                        <option value="kentucky">Kentucky</option>
+                        <option value="louisiana">Louisiana</option>
+                        <option value="maine">Maine</option>
+                        <option value="maryland">Maryland</option>
+                        <option value="massachusetts">Massachusetts</option>
+                        <option value="michigan">Michigan</option>
+                        <option value="minnesota">Minnesota</option>
+                        <option value="mississippi">Mississippi</option>
+                        <option value="missouri">Missouri</option>
+                        <option value="montana">Montana</option>
+                        <option value="nebraska">Nebraska</option>
+                        <option value="nevada">Nevada</option>
+                        <option value="new_hampshire">New Hampshire</option>
+                        <option value="new_jersey">New Jersey</option>
+                        <option value="new_mexico">New Mexico</option>
+                        <option value="new_york">New York</option>
+                        <option value="north_carolina">North Carolina</option>
+                        <option value="north_dakota">North Dakota</option>
+                        <option value="ohio">Ohio</option>
+                        <option value="oklahoma">Oklahoma</option>
+                        <option value="oregon">Oregon</option>
+                        <option value="pennsylvania">Pennsylvania</option>
+                        <option value="rhode_island">Rhode Island</option>
+                        <option value="south_carolina">South Carolina</option>
+                        <option value="south_dakota">South Dakota</option>
+                        <option value="tennessee">Tennessee</option>
+                        <option value="texas">Texas</option>
+                        <option value="utah">Utah</option>
+                        <option value="vermont">Vermont</option>
+                        <option value="virginia">Virginia</option>
+                        <option value="washington">Washington</option>
+                        <option value="west_virginia">West Virginia</option>
+                        <option value="wisconsin">Wisconsin</option>
+                        <option value="wyoming">Wyoming</option>
+                      </select>
                     </div>
-                        {/* <form>
-                            <div class="form-row">
-                                <div class="col-12 col-md-9 mb-2 mb-md-0"><input class="form-control form-control-lg" type="email" placeholder="Enter your email..." /></div>
-                                <div class="col-12 col-md-3"><button class="btn btn-block btn-lg btn-primary" type="submit">Sign up!</button></div>
-                            </div>
-                        </form> */}
+                  </div>
+                </div>
+                <div class="col-xl-9 mx-auto">
+                  <button className="bg-primary" onClick={this.handleClick}>Find Breweries</button>
                 </div>
             </div>
         </header>
 
-        {/* <label for="userName">Name:</label>
-        <input className="text" name="userName" id="userName" onChange={this.myChangeHandler}></input>
-        <br/> */}
-
-        {/* <label for="userCity">City:</label>
-        <input className="text" name="userCity" id="userCity" onChange={this.myChangeHandler}></input>
-        <br/> */}
-
-        {/* <label for="userType">Type: </label>
-        <select id="userType" name="userType" onChange={this.myChangeHandler}>
-            <option value="">Select A Type</option>
-            <option value="micro">Micro</option>
-            <option value="nano">Nano</option>
-            <option value="regional">Regional</option>
-            <option value="brewpub">Brewpub</option>
-            <option value="large">Large</option>
-            <option value="planning">Planning</option>
-            <option value="bar">Bar</option>
-            <option value="contract">Contract</option>
-            <option value="proprietor">Proprietor</option>
-            <option value="closed">Closed</option>
-          </select><br/> */}
-
-        {/* <label for="userState">State: </label>
-        <select id="userState" name="userState" onChange={this.myChangeHandler}>
-          <option value="">Select a State</option>
-          <option value="alabama">Alabama</option>
-          <option value="alaska">Alaska</option>
-          <option value="arizona">Arizona</option>
-          <option value="arkansas">Arkansas</option>
-          <option value="california">California</option>
-          <option value="colorado">Colorado</option>
-          <option value="connecticut">Connecticut</option>
-          <option value="delaware">Delaware</option>
-          <option value="florida">Florida</option>
-          <option value="georgia">Georgia</option>
-          <option value="hawaii">Hawaii</option>
-          <option value="idaho">Idaho</option>
-          <option value="illinois">Illinois</option>
-          <option value="indiana">Indiana</option>
-          <option value="iowa">Iowa</option>
-          <option value="kansas">Kansas</option>
-          <option value="kentucky">Kentucky</option>
-          <option value="louisiana">Louisiana</option>
-          <option value="maine">Maine</option>
-          <option value="maryland">Maryland</option>
-          <option value="massachusetts">Massachusetts</option>
-          <option value="michigan">Michigan</option>
-          <option value="minnesota">Minnesota</option>
-          <option value="mississippi">Mississippi</option>
-          <option value="missouri">Missouri</option>
-          <option value="montana">Montana</option>
-          <option value="nebraska">Nebraska</option>
-          <option value="nevada">Nevada</option>
-          <option value="new_hampshire">New Hampshire</option>
-          <option value="new_jersey">New Jersey</option>
-          <option value="new_mexico">New Mexico</option>
-          <option value="new_york">New York</option>
-          <option value="north_carolina">North Carolina</option>
-          <option value="north_dakota">North Dakota</option>
-          <option value="ohio">Ohio</option>
-          <option value="oklahoma">Oklahoma</option>
-          <option value="oregon">Oregon</option>
-          <option value="pennsylvania">Pennsylvania</option>
-          <option value="rhode_island">Rhode Island</option>
-          <option value="south_carolina">South Carolina</option>
-          <option value="south_dakota">South Dakota</option>
-          <option value="tennessee">Tennessee</option>
-          <option value="texas">Texas</option>
-          <option value="utah">Utah</option>
-          <option value="vermont">Vermont</option>
-          <option value="virginia">Virginia</option>
-          <option value="washington">Washington</option>
-          <option value="west_virginia">West Virginia</option>
-          <option value="wisconsin">Wisconsin</option>
-          <option value="wyoming">Wyoming</option>
-        </select><br/> */}
-        <button className="button" onClick={this.handleClick}>Click</button>
         <Description description={this.state.description} />
+        
+        <footer class="footer bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+                        <ul class="list-inline mb-2">
+                            <li class="list-inline-item"><p>Credit to Open Brewery DB by Chris Mears</p></li>
+                        </ul>
+                        <p class="text-muted small mb-4 mb-lg-0">Page Created by Ryan Lasauskas</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </footer>
       </div>
     )
   }
--- Project/apitest/src/components/desc.js
+++ Project/apitest/src/components/desc.js
@@ -3,25 +3,26 @@ import React from 'react';
 const Description = ({description}) => {
   return(
     <div>
-      <center><h1>Brewery Data</h1></center>
       {description.map((brewery) => (
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">{brewery.name}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">ID#: {brewery.id}</h6>
-            <p className="card-text">Type: {brewery.brewery_type}</p>
-            <p className="card-text">Address: {brewery.street}</p>
-            <p className="card-text">City: {brewery.city}</p>
-            <p className="card-text">State: {brewery.state}</p>
-            <p className="card-text">Postal: {brewery.postal_code}</p>
-            <p className="card-text">Country: {brewery.country}</p>
-            <p className="card-text">Phone: {brewery.phone}</p>
-            <p className="card-text">Website: <a href={brewery.website_url}>{brewery.website_url}</a></p>
-            <p className="card-text">Last Database Update: {brewery.updated_at}</p>
-            
-            
-          </div>  
-        </div>  
+         <section class="showcase">
+           <div class="container-fluid p-0">
+             <div class="row no-gutters">
+                <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                  <h2>{brewery.name}</h2>
+                  <h4><strong>ID#: </strong>{brewery.id}</h4>
+                  <p><strong>Type: </strong>{brewery.brewery_type}</p>
+                  <p><strong>Address: </strong>{brewery.street}</p>
+                  <p><strong>City: </strong>{brewery.city}</p>
+                  <p><strong>State: </strong>{brewery.state}</p>
+                  <p><strong>Postal: </strong>{brewery.postal_code}</p>
+                  <p><strong>Country: </strong>{brewery.country}</p>
+                  <p><strong>Phone: </strong>{brewery.phone}</p>
+                  <p><strong>Website: </strong><a href={brewery.website_url}>{brewery.website_url}</a></p>
+                  <p><strong>Last Database Update: </strong>{brewery.updated_at}</p>
+                </div>
+             </div>
+           </div>
+         </section>
       ))}
     </div>
   )
